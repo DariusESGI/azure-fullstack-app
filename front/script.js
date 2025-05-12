eb65ba2e64e60f882f24aecc10785ac121f3167a
Darius Courtiade <darius.courtiade@gmail.com>
2025-05-12 17:07:27 +0200

    Mise à jour du script front pour appel dynamique à l'API

--- front/script.js
+++ front/script.js
@@ -1,3 +1,16 @@
 function direBonjour() {
-    alert("Bonjour depuis Azure !");
+    fetch('https://backendfullstack-a5hhf8axd3cwcyf8.canadacentral-01.azurewebsites.net/api/hello')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Erreur HTTP " + response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            alert(data.message);
+        })
+        .catch(error => {
+            alert("Erreur API : " + error.message);
+            console.error(error);
+        });
 }
