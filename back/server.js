7c359403c326bf48bf92ded91ee7ff0f33d9bb02
Darius Courtiade <darius.courtiade@gmail.com>
2025-05-12 15:13:39 +0200

    Ajout de la connexion réelle à Azure SQL

--- back/server.js
+++ back/server.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const sql = require('mssql');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Bonjour depuis le back déployé sur Azure !' });
+// Configuration de la base de données
+const config = {
+  user: 'ESGI2A1',
+  password: 'Azerty123456789!',
+  server: 'sql-fullstack-server.database.windows.net',
+  database: 'fullstack-database',
+  options: {
+    encrypt: true,              // Obligatoire pour Azure
+    trustServerCertificate: false
+  }
+};
+
+// Route API pour lire les messages
+app.get('/api/hello', async (req, res) => {
+  try {
+    await sql.connect(config);
+    const result = await sql.query`SELECT TOP 1 content FROM messages ORDER BY id DESC`;
+    res.json({ message: result.recordset[0].content });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erreur serveur');
+  }
 });
 
 app.listen(port, () => {
