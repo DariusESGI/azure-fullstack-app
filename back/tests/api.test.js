d83b4b8f3bc65c90ee56d30409b126b950cff3d5
Darius Courtiade <darius.courtiade@gmail.com>
2025-05-13 11:15:04 +0200

    Ajout des tests automatisés pour l'API

---
+++ back/tests/api.test.js
@@ -0,0 +1,7 @@
+const axios = require('axios');
+
+test('GET /api/hello renvoie un message texte', async () => {
+  const response = await axios.get('https://backendfullstack-a5hhf8axd3cwcyf8.canadacentral-01.azurewebsites.net/api/hello');
+  expect(response.status).toBe(200);
+  expect(typeof response.data.message).toBe('string');
+});
