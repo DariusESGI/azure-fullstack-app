d15bf314c99851b15be6b4e4b8ad3cfe4b4473a6
DariusESGI <162986042+DariusESGI@users.noreply.github.com>
2025-05-13 14:55:57 +0200

    Update api.test.js

--- back/tests/api.test.js
+++ back/tests/api.test.js
@@ -4,4 +4,4 @@ test('GET /api/hello renvoie un message texte', async () => {
   const response = await axios.get('https://backendfullstack-a5hhf8axd3cwcyf8.canadacentral-01.azurewebsites.net/api/hello');
   expect(response.status).toBe(200);
   expect(typeof response.data.message).toBe('string');
-}, 10000); // ⬅️ délai max du test fixé à 10 secondes
+}, 100000); 
